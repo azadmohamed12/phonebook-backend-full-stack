4b4ea87a6f6d7728bf1629a15134777f3b1f3962
azadmohamed12 <hirsiwarsame0@gmail.com>
2025-06-26 01:46:48 -0700

    📦 Initial commit for phonebook backend

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+
+const morgan = require('morgan')
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(express.json())
+app.use(cors())
+app.use(requestLogger)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+
+
+app.get(`/api/info`, (request,response)=>{
+  const numberPerson = persons.length
+const date = new Date();
+const message = `Phonebook has info ${numberPerson} people ${date}`
+response.send(`<h1>${message}</h1>`)
+})
+
+app.get(`/api/persons`,(request,response)=>{
+  response.json(persons)
+})
+
+app.get(`/api/persons/:id`,(request,response)=>{
+const id = request.params.id
+const person = persons.find((person)=> person.id === id)
+if(person){
+  response.json(person)
+}else{
+  response.status(404).json({Error:"person not found"})
+}
+})
+
+app.delete(`/api/persons/:id`,(request,response)=>{
+  const id = request.params.id
+  persons = persons.filter((p)=> p.id !== id)
+response.status(204).end()
+  
+})
+
+app.post(`/api/persons`,(request,response)=>{
+   const person = request.body
+
+   if(!person.name){
+   return response.status(400).json({error:'name is missing'})
+  }
+  if(!person.number){
+   return response.status(400).json({error:"number is missing"})
+  }
+const nameNormal = person.name.trim().toLowerCase()
+const nameExist = persons.some((p)=> p.name.trim().toLowerCase() === nameNormal)
+
+  if(nameExist){
+return response.status(400).json({error:"name must be unique"})
+  }
+
+   const maxId = persons.length> 0 ? Math.max(...persons.map((p)=> Number(p.id))):0
+  const newPerson = 
+    {
+...person,
+id: String(maxId + 1)
+    }
+  persons = persons.concat(newPerson)
+  console.log(newPerson)
+  response.json(newPerson)
+
+  
+ 
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT =  process.env.PORT  || 3001;
+app.listen(PORT, ()=>{
+  console.log(`serving is running on port ${PORT}`)
+})
