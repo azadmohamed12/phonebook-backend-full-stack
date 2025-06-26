cb8d1ad847c8bfec731b1e7cadadc057ce763be9
azadmohamed12 <hirsiwarsame0@gmail.com>
2025-06-26 12:16:25 -0700

    Add backend files

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
+
 app.use(express.json())
 
 
 app.use(express.static('dist'))
 
-
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
 
 let persons = [
     { 
